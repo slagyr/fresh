9ebc969dea9e19da6ced83d53a431b9e768a69e3
Micah <micah@8thlight.com>
2011-01-17 23:52:24 -0600

    updated project version and dev dependencies

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject fresh "1.0.0-SNAPSHOT"
+(defproject fresh "1.0.0"
   :description "A library to keep your clojure runtime 'Fresh'."
   :dependencies [[org.clojure/clojure "1.2.0"]]
-  :dev-dependencies [[speclj "1.2.0"]]
+  :dev-dependencies [[speclj "1.2.0"]
+                     [lein-clojars "0.6.0"]]
   :test-path "spec"
   :dev-resources-path "example_src"
 
